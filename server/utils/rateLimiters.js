957a67669c1f3c10fa930a5a76fb790f4d64d0c0
jayeshpal001 <jayashpal5@gmail.com>
2026-02-05 01:15:36 +0530

    rateLimiter update

--- server/utils/rateLimiters.js
+++ server/utils/rateLimiters.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const sendOtpLimiter = rateLimit({
     windowMs: 10*60*1000, 
-    max: 3, 
+    max: 20, 
     message: "Too many otp attempts. Please try again in 10 minutes."
 })
 
