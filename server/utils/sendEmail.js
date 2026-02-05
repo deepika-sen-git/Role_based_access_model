6cdc3f79ea4a794bd9dc178d4470f7b09c675b2b
jayeshpal001 <jayashpal5@gmail.com>
2026-02-05 23:17:28 +0530

    tranporter update

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,32 +1,34 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, html) => {
-    try {
-        if(!to || !subject || !html){
-            res.status(400).json({
-                message: "Missing required email fields"
-            })
-        }
-        const transporter = nodemailer.createTransport({
-            service: "gmail", 
-            auth: {
-                user: process.env.EMAIL, 
-                pass: process.env.PASS
-            }
-        }); 
-
-        const mailOption = {
-            from: `Hospital Appointment <jayashpal3@gmail.com>`, 
-            to, 
-            subject, 
-            html
-        }
-
-        transporter.sendMail(mailOption); 
-    } catch (error) {
-        console.log(error.message);
-        
+  try {
+    if (!to || !subject || !html) {
+      res.status(400).json({
+        message: "Missing required email fields",
+      });
     }
-}
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
 
-module.exports = sendEmail; 
+    const mailOption = {
+      from: `Hospital Appointment <jayashpal3@gmail.com>`,
+      to,
+      subject,
+      html,
+    };
+
+    transporter.sendMail(mailOption);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = sendEmail;
